eda18d127322bcd19cd9fde9c3dabc7bfdfae8b7
Muneeb Abbasi <abbasimuneeb7869@gmail.com>
2023-08-09 11:37:43 +0100

    Add files via upload

---
+++ webcam/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Web Cam</title>
+  <!-- Include Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+</head>
+<style>
+  body {
+    background-color: rgb(165, 42, 42);
+
+  }
+
+  .parent {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
+
+<body>
+  <div class="parent">
+    <div class="text-white">
+      <h1>
+        Capture a Photo from Webcam <br> using Ajax and save photo in folder
+        using PHP <br>
+        <a href="capture-photo-with-webcam.php">
+          <button class="btn btn-warning btn-lg">Capture Photo</button></a>
+      </h1>
+    </div>
+  </div>
+
+  <!-- Include Bootstrap JS (optional) -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
